605b66e66799057ade985f7e5ebdf8af08498488
Koyojin <143325502+Koyojin@users.noreply.github.com>
2023-10-12 16:29:27 +0900

    [Silver III] Title: 소수 구하기, Time: 228 ms, Memory: 46468 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,21 +1,18 @@
 import sys
-import math
 
-def is_prime(x):
-    # 1은 소수가 아니므로 False 반환
-    if x == 1:
-        return False
-    # x가 2부터 int(x의 제곱근)+1 까지의 수로 나누어떨어지면 소수가 아니므로 False 반환
-    for i in range(2, int(math.sqrt(x)+1)):
-        if x % i == 0:
-            return False
-    # 위의 조건에 해당하지 않으면 소수이므로 True 반환
-    return True
+def prime(n):
+    is_prime=[0,0]+[1]*(n-1)
+    primes=[]
+    for i in range(2,n+1):
+        if is_prime[i]:
+            primes.append(i)
+            for k in range(i*2,n+1,i):
+                is_prime[k]=0
 
-# 입력받은 두 수의 범위를 저장
-m, n = map(int, sys.stdin.readline().split())
+    return primes
 
-# m부터 n까지의 수 중에서 소수인 수를 출력
-for i in range(m, n+1):
-    if is_prime(i):
+m,n=map(int,input().split())
+primes=prime(n)
+for i in primes:
+    if i>=m:
         print(i)
