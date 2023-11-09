2b78f5a3d6d75388341fed88fcf98e1b1c3a34c9
Koyojin <143325502+Koyojin@users.noreply.github.com>
2023-11-10 01:01:03 +0900

    [Gold IV] Title: 오큰수, Time: 892 ms, Memory: 202408 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+res=[-1]*n
+stack=[]
+for i in range(n):
+   
+    while stack and arr[stack[-1]]<arr[i]:
+        res[stack.pop()]=arr[i]
+    stack.append(i)
+  
+print(' '.join(map(str,res)))
