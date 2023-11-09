ec44420b4277370ea99ae7a1f41aef4059dd9757
Koyojin <143325502+Koyojin@users.noreply.github.com>
2023-11-09 12:50:11 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+n=int(input())
+q=deque([])
+def que(s):
+    if s.startswith("pu"):
+        _,b=s.split()
+        q.append(b)
+    elif s.startswith("si"):
+        print(len(q))
+    elif s.startswith("e"):
+        if len(q)==0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(q)==0:
+            print(-1)
+        else:
+            if s.startswith("po"):
+                print(q.popleft())
+            if s.startswith("f"):
+                print(q[0])
+            if s.startswith("b"):
+                print(q[-1])
+for i in range(n):
+    s=input().rstrip()
+    que(s)
