6aec0d008a41460571717da0076decd4e69dc674
Koyojin <143325502+Koyojin@users.noreply.github.com>
2023-11-26 19:54:05 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,9 +1,22 @@
+import sys
+input=sys.stdin.readline
+
 n=int(input())
-dp=[]
-for i in range(n):
-    dp.append(list(map(int,input().split())))
-for i in range(1,n):
-    dp[i][0]=min(dp[i-1][1],dp[i-1][2])+dp[i][0]
-    dp[i][1]=min(dp[i-1][0],dp[i-1][2])+dp[i][1]
-    dp[i][2]=min(dp[i-1][0],dp[i-1][1])+dp[i][2]
-print(min(dp[n-1]))
+r,g,b=map(int,input().split())
+R=[0,r]
+G=[0,g]
+B=[0,b]
+dpR=[0,r]+[0]*(n-1)
+dpG=[0,g]+[0]*(n-1)
+dpB=[0,b]+[0]*(n-1)
+
+for i in range(2,n+1):
+    a,b,c=map(int,input().split())
+    R.append(a)
+    G.append(b)
+    B.append(c)
+    dpR[i]=min(R[i]+dpG[i-1],R[i]+dpB[i-1])
+    dpG[i]=min(G[i]+dpR[i-1],G[i]+dpB[i-1])
+    dpB[i]=min(B[i]+dpR[i-1],B[i]+dpG[i-1])
+
+print(min(dpR[n],dpG[n],dpB[n]))
