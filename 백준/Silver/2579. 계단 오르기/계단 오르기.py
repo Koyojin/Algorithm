98ae0fe2d6c1891272919ffd957f17cd0523a579
Koyojin <143325502+Koyojin@users.noreply.github.com>
2024-05-29 20:02:17 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,22 +1,19 @@
 n=int(input())
-mm=[0]*(n+1)
-value=[0]*(n+1)
+arr=[0]
+dp=[0]
 
-for i in range(1,n+1):
-    value[i]=int(input())
+for _ in range(n):
+    a=int(input())
+    arr.append(a)
+    dp.append(a)
 
 if n==1:
-    print(value[1])
-    exit()
+    print(arr[1])
+else:
+    dp[1]=arr[1]
+    dp[2]=arr[1]+arr[2]
 
-elif n==2:
-    print(value[1]+value[2])
-    exit()
+    for i in range(3,n+1):
+        dp[i]=max(arr[i]+arr[i-1]+dp[i-3],arr[i]+dp[i-2])
 
-mm[1]=value[1]
-mm[2]=value[1]+value[2]
-
-for i in range(3,n+1):
-    mm[i]=value[i]+max(mm[i-2],mm[i-3]+value[i-1])
-
-print(mm[-1])
+    print(dp[n])    
